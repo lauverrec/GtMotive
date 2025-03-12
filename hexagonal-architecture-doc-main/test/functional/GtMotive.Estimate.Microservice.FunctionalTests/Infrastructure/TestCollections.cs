b90b07779145de37d933a926c67f913aa7d05765
lauverrec <lauraverarec@gmail.com>
2025-03-12 19:14:46 +0100

    deleted

--- hexagonal-architecture-doc-main/test/functional/GtMotive.Estimate.Microservice.FunctionalTests/Infrastructure/TestCollections.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GtMotive.Estimate.Microservice.FunctionalTests.Infrastructure
-{
-    public static class TestCollections
-    {
-        public const string Functional = "Functional";
-    }
-}
--- hexagonal-architecture-doc-main/test/infrastructure/GtMotive.Estimate.Microservice.InfrastructureTests/Infrastructure/TestCollections.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GtMotive.Estimate.Microservice.InfrastructureTests.Infrastructure
-{
-    public static class TestCollections
-    {
-        public const string TestServer = "TestServer";
-    }
-}
