519c9b3c486956d386ca1a204dee3022ad4dbc31
lauverrec <lauraverarec@gmail.com>
2025-03-12 19:16:01 +0100

    deleted

--- hexagonal-architecture-doc-main/test/infrastructure/GtMotive.Estimate.Microservice.InfrastructureTests/Infrastructure/TestServerCollectionFixture.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace GtMotive.Estimate.Microservice.InfrastructureTests.Infrastructure
-{
-    [CollectionDefinition(TestCollections.TestServer)]
-    public class TestServerCollectionFixture : ICollectionFixture<GenericInfrastructureTestServerFixture>
-    {
-    }
-}
